89f6e32c15762aafb5fa536d78ddb1d751026078
Glitch (arras-template-with-linkedlists-and-anti-lag) <none>
2021-09-08 15:37:17 +0000

    🔨🚃 Checkpoint ./server.js:24116714/375690

--- server.js
+++ server.js
@@ -5284,25 +5284,37 @@ var gameloop = (() => {
     my.updateAABB(my.activation.check());
   }
   const portalSuck = (() => {
-    var { width, height, xgrid, ygrid } = room;
+    var { width, height, xgrid, ygrid, setup } = room;
     var tpd = Math.min(width / xgrid, height / ygrid) / 8;
     tpd *= tpd;
-    var { sqrt } = Math;
+    var { sqrt, random } = Math;
+    var ports = () => {
+      var l = [],
+        i,
+        j;
+      for (j = 0; j < ygrid; j++)
+        for (i = 0; i < xgrid; i++)
+          if ("port" === setup[j][i])
+            l.push((width * (0.5 + i)) / xgrid, (height * (0.5 + j)) / ygrid);
+      return l;
+    };
+    var portsCount = ports.length >> 1;
     return e => {
       var { x, y } = e;
       if (x < 0 || y < 0 || x >= width || y >= height) return;
       var i = ((xgrid * x) / width) >> 0,
         j = ((ygrid * y) / height) >> 0;
-      if ("port" !== room.setup[j][i]) return;
+      if ("port" !== setup[j][i]) return;
       i = (width * (0.5 + i)) / xgrid - x;
       j = (height * (0.5 + j)) / ygrid - y;
       var m = i * i + j * j;
       if (m < tpd) {
-        e.x = 0;
-        e.y = 0;
+        var rp = (portsCount * random()) << 1;
+        e.x = ports[rp];
+        e.y = ports[1 + rp];
         return;
       }
-      m = 10 / sqrt(m);
+      m = 2 / sqrt(m);
       var { accel } = e;
       accel.x += m * i;
       accel.y += m * j;
